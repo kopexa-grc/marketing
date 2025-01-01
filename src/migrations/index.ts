181ba413e592e9f86250a5ccbb47f7e8d543086a
Julian Koehn <me@julian.pro>
2025-01-01 18:39:24 +0100

    fix(hero): migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250101_171607 from './20250101_171607';
+import * as migration_20250101_173915 from './20250101_173915';
 
 export const migrations = [
   {
     up: migration_20250101_171607.up,
     down: migration_20250101_171607.down,
-    name: '20250101_171607'
+    name: '20250101_171607',
+  },
+  {
+    up: migration_20250101_173915.up,
+    down: migration_20250101_173915.down,
+    name: '20250101_173915'
   },
 ];
