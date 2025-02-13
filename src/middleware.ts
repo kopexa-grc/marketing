f51f9b49d59a0abec7c55c9916981a67d2ab019d
Julian Koehn <me@julian.pro>
2025-02-13 23:46:52 +0100

    fix: sitemap

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,6 +30,6 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|logo.svg).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|sitemap-0.xml|robots.txt|logo.svg).*)",
   ],
 };
